31be8517b4004807f2ef5bfc2d37ce20927aad0d
exejar <56853437+exejar@users.noreply.github.com>
2023-05-19 21:44:17 -0400

    Added viewport height and width units

--- src/main/kotlin/club/maxstats/kolour/gui/MeasurementUnit.kt
+++ src/main/kotlin/club/maxstats/kolour/gui/MeasurementUnit.kt
@@ -23,13 +23,37 @@ data class PercentUnit(val percent: Float): MeasurementUnit(percent) {
         return full * (percent / 100)
     }
 }
+data class ViewportWidthUnit(val vw: Float): MeasurementUnit(vw) {
+    /**
+     * Converts the viewport width (vw) value to pixels based on the specified viewport width.
+     *
+     * @param viewWidth The width of the viewport in pixels.
+     * @return The calculated pixel value based on the viewport width value.
+     */
+    fun toPixels(viewWidth: Float): Float {
+        return viewWidth * (vw / 100)
+    }
+}
+data class ViewportHeightUnit(val vh: Float): MeasurementUnit(vh) {
+    /**
+     * Converts the viewport height (vh) value to pixels based on the specified viewport height.
+     *
+     * @param viewHeight The height of the viewport in pixels.
+     * @return The calculated pixel value based on the viewport height value.
+     */
+    fun toPixels(viewHeight: Float): Float {
+        return viewHeight * (vh / 100)
+    }
+}
 data class PixelUnit(val pixel: Float): MeasurementUnit(pixel)
 
 val Number.rem: RemUnit
     get() = RemUnit(this.toFloat())
-
 val Number.percent: PercentUnit
     get() = PercentUnit(this.toFloat())
-
+val Number.vw: ViewportWidthUnit
+    get() = ViewportWidthUnit(this.toFloat())
+val Number.vh: ViewportHeightUnit
+    get() = ViewportHeightUnit(this.toFloat())
 val Number.px: PixelUnit
     get() = PixelUnit(this.toFloat())
