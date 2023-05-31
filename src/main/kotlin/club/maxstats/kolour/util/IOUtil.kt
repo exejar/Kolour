9ebf67002dd04c7b50bd6eadd7f87e1b4cb7cdde
Max <56853437+exejar@users.noreply.github.com>
2023-05-31 01:18:05 -0400

    Changed this::class to Main::class to properly retrieve the class loader

--- src/main/kotlin/club/maxstats/kolour/util/IOUtil.kt
+++ src/main/kotlin/club/maxstats/kolour/util/IOUtil.kt
@@ -1,8 +1,9 @@
 package club.maxstats.kolour.util
 
+import club.maxstats.kolour.Main
 import java.io.InputStream
 
-fun String.asResource(): InputStream = this::class.java.classLoader.getResourceAsStream(this)
+fun String.asResource(): InputStream = Main::class.java.classLoader.getResourceAsStream(this)
     ?: throw IllegalStateException("Failed to fetch resource $this")
 
 fun InputStream.readToString() = this.readBytes().toString(Charsets.UTF_8)
